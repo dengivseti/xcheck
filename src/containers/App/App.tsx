dee8e02ccd90b11973ace545cd8f0b92d6cb7133
Dmitrii <dengivseti@list.ru>
2020-09-04 17:50:11 +0300

    init: start xcheck

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import styles from './App.module.scss';
+
+const App = () => {
+  return (
+    <main className={styles.app}>
+      <Button type="primary">Hello, Ant Design!</Button>
+      <a href="foo.bar">I'm a link. Click me, please!</a>
+    </main>
+  );
+};
+
+export default App;
