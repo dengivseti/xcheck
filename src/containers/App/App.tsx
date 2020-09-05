86a135138f56672adb14069de4179d32dabe2a0d
Dmitrii <dengivseti@list.ru>
2020-09-05 20:57:54 +0300

    feat: add auth page

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -4,19 +4,11 @@ import { useDispatch } from 'react-redux';
 import { Button } from 'antd';
 import { setUser } from '../../redux/slices/authSlice';
 import styles from './App.module.scss';
+import { Auth } from '../../pages/Auth';
 
 const App = () => {
   const dispatch = useDispatch();
 
-  return (
-    <main className={styles.app}>
-      <Button type="primary" onClick={() => dispatch(setUser('vova'))}>
-        Hello, Ant Design!
-      </Button>
-      <a href="foo.bar">I'm a link. Click me, please!</a>
-      <h1>Hello</h1>
-    </main>
-  );
+  return <Auth />;
 };
-
 export default App;
