28a60faeca2a4ca405c8ae549801c47ea4dabbc8
dengivseti <dengivseti@list.ru>
2020-09-11 16:32:06 +0300

    Revert "Feat/cross check"

--- src/utils/values.ts
+++
@@ -1,7 +0,0 @@
-import { ISelectTypeRoles } from '../interfaces/interfaces';
-
-export const listSelectTypeRoles: ISelectTypeRoles[] = [
-  { value: 'author', name: 'Author' },
-  { value: 'student', name: 'Student' },
-  { value: 'supervisor', name: 'Supervisor' },
-];
