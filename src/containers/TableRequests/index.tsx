ee97408a5388e2c93f76cf06deec6fa8f26f1156
Dmitrii <dengivseti@list.ru>
2020-09-11 18:38:21 +0300

    feat: add table requests

---
+++ src/containers/TableRequests/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Table } from 'antd';
+import { data } from './mockData';
+
+export const TableRequests: React.FC = () => {
+  const columns: any = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+    },
+    {
+      title: 'CrossCheck',
+      dataIndex: 'crossCheckSessionId',
+      sorter: (a: any, b: any) =>
+        a.crossCheckSessionId.length - b.crossCheckSessionId.length,
+      sortDirections: ['descend', 'ascend'],
+    },
+    {
+      title: 'Author',
+      dataIndex: 'author',
+      sorter: (a: any, b: any) => a.author.length - b.author.length,
+      sortDirections: ['descend', 'ascend'],
+    },
+    {
+      title: 'Task',
+      dataIndex: 'task',
+      sorter: (a: any, b: any) => a.task.length - b.task.length,
+      sortDirections: ['descend', 'ascend'],
+    },
+    {
+      title: 'State',
+      dataIndex: 'state',
+      filters: [
+        { text: 'DRAFT', value: 'DRAFT' },
+        { text: 'PUBLISHED', value: 'PUBLISHED' },
+        { text: 'COMPLETED', value: 'COMPLETED' },
+      ],
+      sorter: (a: any, b: any) => a.state.length - b.state.length,
+      sortDirections: ['descend', 'ascend'],
+      onFilter: (value, record) => record.state.indexOf(value) === 0,
+    },
+    {
+      title: 'SelfGrade',
+      dataIndex: 'selfGradeScore',
+      sorter: (a: any, b: any) => a.selfGradeScore - b.selfGradeScore,
+      sortDirections: ['descend', 'ascend'],
+    },
+  ];
+
+  return (
+    <>
+      <Table columns={columns} dataSource={data} />
+    </>
+  );
+};
---
+++ src/containers/TableRequests/mockData.ts
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    id: 'rev-req-1',
+    crossCheckSessionId: 'rss2020Q3react-xcheck', // may be null if this review is not a part of any session
+    author: 'cardamo',
+    task: 'simple-task-v1',
+    state: 'PUBLISHED', // enum [DRAFT, PUBLISHED, COMPLETED]
+    selfGrade: {},
+    selfGradeScore: 550,
+  },
+  {
+    id: 'rev-req-2',
+    crossCheckSessionId: 'rss2020Q3react-xcheck', // may be null if this review is not a part of any session
+    author: 'card',
+    task: 'simple-task-v1',
+    state: 'PUBLISHED', // enum [DRAFT, PUBLISHED, COMPLETED]
+    selfGrade: {},
+    selfGradeScore: 400,
+  },
+  {
+    id: 'rev-req-3',
+    crossCheckSessionId: 'rss2020Q3react-xcheck', // may be null if this review is not a part of any session
+    author: 'gsgr',
+    task: 'simple-task-v55',
+    state: 'DRAFT', // enum [DRAFT, PUBLISHED, COMPLETED]
+    selfGrade: {},
+    selfGradeScore: 650,
+  },
+  {
+    id: 'rev-req-4',
+    crossCheckSessionId: 'rss2020Q3react-xcheck', // may be null if this review is not a part of any session
+    author: 'feaw',
+    task: 'simple-task-v1',
+    state: 'DRAFT', // enum [DRAFT, PUBLISHED, COMPLETED]
+    selfGrade: {},
+    selfGradeScore: 350,
+  },
+  {
+    id: 'rev-req-5',
+    crossCheckSessionId: 'rss2020Q3react-xcheck', // may be null if this review is not a part of any session
+    author: 'keitw',
+    task: 'simple-task-v33',
+    state: 'PUBLISHED', // enum [DRAFT, PUBLISHED, COMPLETED]
+    selfGrade: {},
+    selfGradeScore: 185,
+  },
+  {
+    id: 'rev-req-6',
+    crossCheckSessionId: 'rss2020Q3react-xcheck', // may be null if this review is not a part of any session
+    author: 'carwtw',
+    task: 'simple-task-v1',
+    state: 'PUBLISHED', // enum [DRAFT, PUBLISHED, COMPLETED]
+    selfGrade: {},
+    selfGradeScore: 575,
+  },
+  {
+    id: 'rev-req-7',
+    crossCheckSessionId: 'rss2020Q3react-xcheck', // may be null if this review is not a part of any session
+    author: 'cardamo',
+    task: 'simple-task-v5',
+    state: 'COMPLETED', // enum [DRAFT, PUBLISHED, COMPLETED]
+    selfGrade: {},
+    selfGradeScore: 550,
+  },
+];
