8b6361ec19e602abaf39c27109123bd8b0cde09e
Dmitrii <dengivseti@list.ru>
2020-09-11 18:45:15 +0300

    fix: bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,25 @@ import { Provider } from 'react-redux';
 
 import store from './redux/store';
 import App from './containers/App/App';
+import { Requests } from './pages/Requests';
+
+const categories = ['Basic Scope', 'Extra Scope', 'Fines'];
+
+const TagHandler = (tags) => {
+  console.log(tags);
+};
+
+const ItemsHandler = (items) => {
+  console.log(items);
+};
 
 ReactDOM.render(
   <>
     <Provider store={store}>
-      <App />
+      {/*<TagCategoriesOrder categories={categories} onTag={TagHandler} />*/}
+      {/*<FormCheckEdit categories={categories} onItems={ItemsHandler} />*/}
+      {/*<CrossCheck />*/}
+      <Requests />
     </Provider>
   </>,
   document.getElementById('root')
