2d1c094d3a5664936bb45c9ecf9f76279c1e6e5c
Dmitrii <dengivseti@list.ru>
2020-09-16 19:31:08 +0300

    refactor: change slice and structure of the project

--- src/containers/FormReview/index.tsx
+++ src/containers/FormReview/index.tsx
@@ -8,114 +8,89 @@ import { Button, Divider, Form, Select, Typography } from 'antd';
 import { Grade } from '../Grade';
 import { IReview } from '../../interfaces/interfaces';
 import { listStateReviews } from '../../utils/values';
-import { fetchRequest, sendReview } from '../../redux/slices/formReviewSlice';
+import { fetchRequest } from '../../redux/slices/requestsSlice';
+import { fetchReview, saveReview } from '../../redux/slices/reviewsSlice';
 import { ListGrade } from '../../components/ListGrade';
 
 interface RouteParams {
   id: string;
 }
 
-export const FormReview: React.FC = () => {
+interface IFormReviewProps {
+  isEdit: boolean;
+}
+
+export const FormReview: React.FC<IFormReviewProps> = (props) => {
+  const { isEdit } = props;
   const params = useParams<RouteParams>();
   const dispatch = useDispatch();
   const history = useHistory();
 
   const author = useSelector((store: RootState) => store.auth.user);
 
-  const { finalGrade, score, totalScore } = useSelector((store: RootState) => {
+  const { finalGrade, score } = useSelector((store: RootState) => {
     return {
       finalGrade: store.grade.finalGrade,
       score: store.grade.score,
-      totalScore: store.grade.totalScore,
     };
   }, shallowEqual);
 
-  const {
-    typeReview,
-    stateReview,
-    idReview,
-    isLoadingReview,
-    idTask,
-    gradeReview,
-  } = useSelector((store: RootState) => {
+  const { review, isLoading } = useSelector((store: RootState) => {
     return {
-      typeReview: store.formCreateReview.type,
-      stateReview: store.formCreateReview.state,
-      idReview: store.formCreateReview.id,
-      isLoadingReview: store.formCreateReview.isLoading,
-      idTask: store.formCreateRequest.idTask,
-      gradeReview: store.formCreateReview.grade,
+      review: store.reviews.review,
+      isLoading: store.reviews.isLoading,
     };
   }, shallowEqual);
 
-  const {
-    titleTask,
-    isLoading,
-    url,
-    items,
-    crossCheckSessionId,
-    selfGrade,
-    idRequest,
-    authorRequest,
-    scoreRequest,
-  } = useSelector((store: RootState) => {
-    return {
-      crossCheckSessionId: store.formCreateRequest.crossCheckSessionId,
-      titleTask: store.formCreateRequest.titleTask,
-      isLoading: store.formCreateRequest.isLoading,
-      url: store.formCreateRequest.url,
-      items: store.formCreateRequest.items,
-      selfGrade: store.formCreateRequest.selfGrade,
-      idRequest: store.formCreateRequest.id,
-      authorRequest: store.formCreateRequest.author,
-      scoreRequest: store.formCreateRequest.score,
-    };
-  }, shallowEqual);
+  const task = useSelector((store: RootState) => store.tasks.task);
+  const request = useSelector((store: RootState) => store.requests.request);
 
   const onFinish = async (value) => {
     const result: IReview = {
       author,
-      crossCheckSessionId,
-      idRequest,
-      idTask,
+      crossCheckSessionId: request.crossCheckSessionId,
+      idRequest: request.id,
+      idTask: request.idTask,
       score,
       state: value.state,
       grade: finalGrade,
     };
-    await dispatch(sendReview(result, typeReview, idReview));
-    history.push('/requests');
+    await dispatch(saveReview(result));
+    history.push('/reviews');
   };
 
   useEffect(() => {
-    dispatch(fetchRequest(params.id, author));
+    if (!task && !request && !review) {
+      dispatch(fetchReview(params.id));
+      if (isEdit) {
+        dispatch(fetchRequest(params.id));
+      }
+    }
   }, []);
-  if (isLoading || isLoadingReview) {
+
+  if (isLoading || !task || !request) {
     return <Loader />;
   }
 
   return (
     <Form layout="horizontal" onFinish={onFinish} autoComplete="off">
       <Typography.Title>
-        {typeReview === 'EDIT' ? 'Редактирование проверки' : 'Проверка'}{' '}
-        {titleTask} от {authorRequest}
+        {isEdit ? 'Редактирование проверки' : 'Проверка'} {task.title} от{' '}
+        {request.author}
       </Typography.Title>
       <Typography.Title level={4}>
         Ссылка на решение:{' '}
-        <Typography.Link href={url} target="_blank">
-          {url}
+        <Typography.Link href={request.url} target="_blank">
+          {request.url}
         </Typography.Link>
       </Typography.Title>
       <h2>Статус</h2>
       <Form.Item
-        initialValue={stateReview}
+        initialValue={isEdit ? review.state : null}
         name="state"
         rules={[{ required: true, message: 'Missing state' }]}
       >
-        <Select
-          placeholder="Выберите статус"
-          defaultValue={stateReview}
-          value={stateReview}
-        >
+        <Select placeholder="Выберите статус">
           {listStateReviews.map((item) => (
             <Select.Option value={item.value} key={item.value}>
               {item.name}
@@ -125,21 +100,21 @@ export const FormReview: React.FC = () => {
       </Form.Item>
       <Divider orientation={'left'}>
         <Typography.Title level={4}>
-          {authorRequest} самопроверка
+          {request.auth} самопроверка
         </Typography.Title>
       </Divider>
-      <ListGrade grade={selfGrade} items={items} />
-      {typeReview === 'EDIT' ? (
+      <ListGrade grade={request.selfGrade} items={task.items} />
+      {isEdit ? (
         <>
-          <Grade type={typeReview} items={items} grade={gradeReview} />
+          <Grade type="EDIT" items={task.items} grade={review.grade} />
         </>
       ) : (
-        <Grade type={typeReview} items={items} grade={selfGrade} />
+        <Grade type="REVIEW" items={task.items} grade={request.selfGrade} />
       )}
 
       <Form.Item>
         <Button type="primary" htmlType="submit">
-          {typeReview === 'EDIT' ? 'Редактировать' : 'Отправить'}
+          {isEdit ? 'Редактировать' : 'Отправить'}
         </Button>
       </Form.Item>
     </Form>
