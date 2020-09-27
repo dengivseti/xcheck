62c5ae6a86c107be06e179499b31f8003769b9a5
DMITRII <dengivseti@list.ru>
2020-09-27 20:05:01 +0300

    fix: bug fix

--- src/containers/FormTask/index.tsx
+++ src/containers/FormTask/index.tsx
@@ -43,7 +43,7 @@ export const FormTask: React.FC = () => {
     if (!task && params.id) {
       dispatch(fetchTask(params.id));
     }
-  }, [params, dispatch, task]);
+  }, [params.id, dispatch, task]);
 
   const onFinish = async (itemsForm) => {
     const value: ITask = {
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
+import App from "./containers/App/App";
 
 ReactDOM.render(
   <>
